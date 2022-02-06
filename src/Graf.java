d44cbd261b89a2c377db31c549269f0504bce039
Nutzu000 <glizard.glizard@yahoo.com>
2022-02-06 19:47:47 +0200

    Implementation

---
+++ src/Graf.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Graf {
+    static String type = "";
+    static int width = 1000;
+    static int height = 1000;
+
+    public Graf() {
+        if (ButtonFrame.type == "Orientat") {
+            type = "Componente tari conexe";
+        } else if (ButtonFrame.type == "Neorientat") {
+            type = "Componente conexe";
+        }
+        JFrame f = new JFrame(type);
+        f.setDefaultCloseOperation(3);
+        f.add(new MyPanel());
+        f.setSize(width, height);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ButtonFrame();
+    }
+}
